cd722d78bc37eb6ee08152858d9348ae8d729976
Henrique Souza <henrique.lsouza2001@gmail.com>
2023-06-01 14:37:09 -0300

    First commit of cdk stack

--- cdk_deploy_for_isolated_ec2/cdk_deploy_for_isolated_ec2_stack.py
+++ cdk_deploy_for_isolated_ec2/cdk_deploy_for_isolated_ec2_stack.py
@@ -1,19 +1,47 @@
 from aws_cdk import (
-    # Duration,
+    Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_events as eventBridge,
+    aws_lambda as lambda_,
+    aws_iam as iam,
+    triggers,
 )
 from constructs import Construct
+from os import path 
 
 class CdkDeployForIsolatedEc2Stack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        lambdaIsolation = lambda_.Function(self, "LambdaForIsolation",
+                runtime = lambda_.Runtime.PYTHON_3_10,
+                handler = "ec2_isolation.lambda_handler",
+                code = lambda_.Code.from_asset(path.join("functions")),
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkDeployForIsolatedEc2Queue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        lambdaStream.add_to_role_policy(iam.PolicyStatement(
+            effect=iam.Effect.ALLOW,
+            actions=[
+                'ec2:*',
+                'autoscaling:*'
+            ],
+            resources=[
+                '*',
+            ],
+        ))
+
+        
+        broker = eventBridge.Rule(self, "guardduty-broker-lambda",
+                         event_pattern=events.EventPattern(
+                                source=["aws.guardduty"]  
+                            )
+                         )
+        broker.add_target(targets.LambdaFunction(lambdaIsolation, retry_attempts=2))
+        
+        trigger = triggers.Trigger(self, "MyTrigger",
+            handler=function_,
+
+            # the properties below are optional
+            execute_after=[broker]
+        )
